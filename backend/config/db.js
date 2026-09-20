e191988e66d0b791df695a22bd080e8b42cca4d7
wahaaah <csavariz29@gmail.com>
2026-09-20 16:50:54 +0800

    Update db.js
    database connection  changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'mysql.railway.internal',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
-    database: process.env.DB_NAME || 'railway',
+    host: process.env.MYSQLHOST || process.env.DB_HOST || 'mysql.railway.internal',
+    user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
+    password: process.env.MYSQLPASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
+    database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10
 });
