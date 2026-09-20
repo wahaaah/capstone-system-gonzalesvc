fa2a715c3d48dd162924bb1c5855cb13e40a9312
wahaaah <csavariz29@gmail.com>
2026-09-20 15:38:52 +0800

    database and api connection changes
    Trying to deploy the backend and frontend system to render, vercel, and railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'mysql.railway.internal',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'gonzales_vision_clinic',
+    password: process.env.DB_PASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
+    database: process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10
 });
