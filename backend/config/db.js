d3003c7690db74607884e5db9684cd448f9c2822
wahaaah <csavariz29@gmail.com>
2026-09-20 15:52:13 +0800

    Update db.js
    changing the db name to railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'mysql.railway.internal',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
-    database: process.env.DB_NAME || 'gonzales_vision_clinic',
+    database: process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10
 });
