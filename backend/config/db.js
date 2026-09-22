35266619a1acce5804722e89637c59a81bbfee0f
wahaaah <csavariz29@gmail.com>
2026-09-22 13:54:57 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || process.env.DB_HOST || 'iriguchi.proxy.rlwy.net',
+    host: process.env.MYSQLHOST || process.env.DB_HOST || 'iriguchi.proxy.rlwy.net:21536',
     port: process.env.MYSQLPORT || process.env.DB_PORT || '3306',
     user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
     password: process.env.MYSQLPASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
