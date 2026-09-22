035e4cac34417da780131015b30bb16b43ae2ffc
wahaaah <csavariz29@gmail.com>
2026-09-22 14:02:41 +0800

    database connection changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
-// config/db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || process.env.DB_HOST || 'iriguchi.proxy.rlwy.net',
-    port: process.env.MYSQLPORT || process.env.DB_PORT || '21536',
-    user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
-    database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME || 'railway',
-    waitForConnections: true,
-    connectionLimit: 10
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
+
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 module.exports = pool;
