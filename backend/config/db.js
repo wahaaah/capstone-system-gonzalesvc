067ce20ef6c5739f9edd63bad44a80a19d5611c9
wahaaah <csavariz29@gmail.com>
2026-09-22 13:59:06 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,6 @@ const pool = mysql.createPool({
     host: process.env.MYSQLHOST || process.env.DB_HOST || 'iriguchi.proxy.rlwy.net',
     port: process.env.MYSQLPORT || process.env.DB_PORT || '21536',
     user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
-    password: process.env.MYSQLPASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
     database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME || 'railway',
     waitForConnections: true,
     connectionLimit: 10
