a3d4cdc62648bc395e32448e672497a1ef54b3af
wahaaah <csavariz29@gmail.com>
2026-09-22 13:53:18 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || process.env.DB_HOST || 'mysql.railway.internal',
+    host: process.env.MYSQLHOST || process.env.DB_HOST || 'iriguchi.proxy.rlwy.net',
+    port: process.env.MYSQLPORT || process.env.DB_PORT || '3306',
     user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
     password: process.env.MYSQLPASSWORD || 'JPKfxDmFHQaVNkMtBySKwpskXNpyeFBx',
     database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME || 'railway',
