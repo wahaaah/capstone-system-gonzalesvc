a7890dd11bc3e544b594ba514784ece856fb5948
wahaaah <csavariz29@gmail.com>
2026-09-22 16:46:09 +0800

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
-const multerStorageCloudinary = require('multer-storage-cloudinary');
-const CloudinaryStorage = multerStorageCloudinary.CloudinaryStorage || multerStorageCloudinary;
+const { CloudinaryStorage } = require('multer-storage-cloudinary-v2');
 const path = require('path');
 
 // Configure Cloudinary using Render environment variables
